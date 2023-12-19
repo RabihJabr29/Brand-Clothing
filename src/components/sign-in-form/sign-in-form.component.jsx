61df4214b9cdca3c61807380866805a2878d98fd
rjabr <rjabr@murex.com>
2023-12-19 14:55:24 +0200

    Added User Context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,5 +1,4 @@
-import "./sign-in-form.styles.scss";
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -9,6 +8,10 @@ import {
   signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils";
 
+import { UserContext } from "../../contexts/user.context";
+
+import "./sign-in-form.styles.scss";
+
 const defaultFormFields = {
   email: "",
   password: "",
@@ -17,6 +20,8 @@ const defaultFormFields = {
 const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
+
+  const { setCurrentUser } = useContext(UserContext);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -34,11 +39,8 @@ const SignInForm = () => {
       alert("Password should be at least 6 characters");
     } else {
       try {
-        const { response } = await signInAuthWithEmailAndPassword(
-          email,
-          password
-        );
-        console.log(response);
+        const { user } = await signInAuthWithEmailAndPassword(email, password);
+        setCurrentUser(user);
         resetFormFields();
       } catch (error) {
         switch (error.code) {
