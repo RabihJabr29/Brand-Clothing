9279789b2b5e6c6c17258ad45341088132f0e699
Rabih Jabr <rabihjabrz2@gmail.com>
2023-12-25 03:20:18 +0200

    Added CartItems to the CartDropdown

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,17 @@
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import "./product-card.styles.scss";
-import React from "react";
+import React, { useContext } from "react";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+
+  const { addItemToCart } = useContext(CartContext);
+
+  const handleAddItem = () => {
+    addItemToCart(product);
+  };
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +19,9 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to Cart</Button>
+      <Button buttonType="inverted" onClick={handleAddItem}>
+        Add to Cart
+      </Button>
     </div>
   );
 };
