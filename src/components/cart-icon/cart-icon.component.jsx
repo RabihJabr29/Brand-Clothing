1533c8864a2c3087e576dcb4a362f7b176ef71fb
Rabih Jabr <rabihjabrz2@gmail.com>
2023-12-26 23:32:51 +0200

    Converted CartIcon to a styled component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,16 @@
-import "./cart-icon.styles.scss";
-
 import React, { useContext } from "react";
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { CartContext } from "../../contexts/cart.context";
+import { CartIconContainer, ItemCount, ShoppingIcon } from "./cart-icon.styles";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, itemsCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
-    <div className="cart-icon-container">
-      <ShoppingIcon className="shopping-icon" onClick={toggleIsCartOpen} />
-      <span className="item-count">{itemsCount}</span>
-    </div>
+    <CartIconContainer>
+      <ShoppingIcon onClick={toggleIsCartOpen} />
+      <ItemCount>{itemsCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
