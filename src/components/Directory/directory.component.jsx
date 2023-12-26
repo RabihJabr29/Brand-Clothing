e82f0db997b4dc0c274528bac0a6df9a68ed3036
Rabih Jabr <rabihjabrz2@gmail.com>
2023-12-26 03:00:57 +0200

    Added Category and fixed styling clashes

--- src/components/Directory/directory.component.jsx
+++ src/components/Directory/directory.component.jsx
@@ -1,11 +1,11 @@
+import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.styles.scss";
-import CategoryItem from "../category-item/category-item.component";
 
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem category={category} key={category.id} />
+        <DirectoryItem directory={category} key={category.id} />
       ))}
     </div>
   );
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,25 +1,14 @@
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 import "./shop.styles.scss";
 
-import React, { useContext, Fragment } from "react";
-
-import { CategoriesContext } from "../../contexts/categories.context";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-
+import { Routes, Route } from "react-router-dom";
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview
-            key={title}
-            title={title}
-            products={products}
-          ></CategoryPreview>
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index path="/" element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
