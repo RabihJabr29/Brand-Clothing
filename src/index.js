80e56a8cb862a5fa04df204ab8a406aec0ca2112
Rabih Jabr <rabihjabrz2@gmail.com>
2023-12-26 03:06:34 +0200

    Small Fixes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./index.scss";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,10 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
-import "./index.scss";
+import { CartProvider } from "./contexts/cart.context";
 
 import reportWebVitals from "./reportWebVitals";
-import { CartProvider } from "./contexts/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
